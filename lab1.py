43951c7aace90141183243d11bd30ea6d48e9941
Vishal1170 <vishalpp283@gmail.com>
2018-08-24 14:23:37 +0530

    initial commit

---
+++ lab1.py
@@ -0,0 +1,5 @@
+emp1=(("First_Name","Vishal"),("Last_Name","Choudhary"),("Company_Name","Relience"),("EmpId","142"),("Join_date","31-10-2017"),{"Desigination":"Manager"})
+for i in emp1:
+    print(i)
+
+    
